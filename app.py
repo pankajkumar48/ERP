5f117708f1478e7ba6f35d12fc7d088489657b8f
Pankaj Kumar <805.bluebell@gmail.com>
2019-07-14 00:17:38 +0530

    AddVendors now adds vendors to DB

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import datetime
 import psycopg2
 
 app = flask.Flask(__name__,static_url_path='',
-            static_folder='static',
-            template_folder='templates')
+			static_folder='static',
+			template_folder='templates')
 app.config["DEBUG"] = True
 
 
@@ -36,6 +36,54 @@ def generateBill():
 @app.route('/allTransactions', methods=['GET'])
 def allTransactions():
 	return render_template('allTransactions.html')
+
+@app.route('/addVendorToDB', methods=['POST'])
+def addVendorToDB():
+	vendorName = request.form.get("vendorName")
+	vendorRate = request.form.get("vendorRate")
+	vendorAddress = request.form.get("vendorAddress")
+	print(f"{vendorName}, {vendorRate} \n {vendorAddress}")
+	return jsonify("successfully received")
+
+@app.route('/addVendor2DB', methods=['POST'])
+def addVendor2DB():
+	vendorName = request.form.get("vendorName")
+	vendorRate = float(request.form.get("vendorRate"))
+	vendorAddress = request.form.get("vendorAddress")
+	cardNo = int(request.form.get("cardNo"))
+	others = request.form.get("others")
+	product = request.form.get("inputProduct")
+
+	if product == 'others':
+		product = others
+	print(vendorName)
+	print(vendorRate)
+	print(vendorAddress)
+	print(cardNo)
+	print(others)
+	print(product)
+	print("Insertion happening")
+	res = "Starting insertion"
+	conn = None
+	try:
+		hostname = 'localhost'
+		username = 'pankaj kumar'
+		password = 'quad2core'
+		database = 'postgres'
+		conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+		cur = conn.cursor()
+		cur.execute("INSERT INTO vendors(cardNo, vendorName, vendorAddress, vendorRate, item) VALUES(%s,%s,%s,%s,%s)",(cardNo, vendorName, vendorAddress, vendorRate, product))
+		conn.commit()
+		cur.close()
+		res = "Successfully Inserted"
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+		res = str(error)
+	finally:
+		if conn is not None:
+			conn.close()
+			#res = "successfully inserted"
+	return jsonify(res)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- testPostgres.py
+++ testPostgres.py
@@ -1,16 +1,24 @@
 import psycopg2
 
 try:
-	conn_string = "host='localhost' dbname='mayur' user='postgres' password='Quad2core@'"
- 
-	# print the connection string we will use to connect
-	print(f"Connecting to database {conn_string}")
- 
-	# get a connection, if a connect cannot be made an exception will be raised here
-	conn = psycopg2.connect(conn_string)
- 
-	# conn.cursor will return a cursor object, you can use this cursor to perform queries
-	cursor = conn.cursor()
-	print("connected")
-except:
-    print("I am unable to connect to the database")
+    # Database credentials
+    hostname = 'localhost'
+    username = 'pankaj kumar'
+    password = 'Quad2core@'
+    database = 'postgres'
+    conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+    # create a new cursor object
+    cur = conn.cursor()
+    # execute the INSERT statement
+    cur.execute("select * from karche;")
+    # commit the changes to the database
+    conn.commit()
+    # close the communication with the PostgresQL database
+    cur.close()
+    res = "Successfully Inserted"
+except (Exception, psycopg2.DatabaseError) as error:
+    print("hi")
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
